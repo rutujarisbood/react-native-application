5f3987773ff4e4964c27d0f935f5b7a74a6d5ae9
Rutuja Risbood <rutujarisbood15@gmail.com>
2022-04-18 19:20:46 -0500

    initial commit

---
+++ config/Firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: 'AIzaSyDFwGnOMupqkpGo3EdsUA8QS1vk5bVQkZ8',
+    authDomain: 'androidmobile-41b4f.firebaseapp.com',
+    databaseURL: 'com.androidmobile',
+    projectId: 'androidmobile-41b4f',
+    storageBucket: 'androidmobile-41b4f.appspot.com',
+    messagingSenderId: '388449292383',
+    appId: '1:70771920103:android:b0549b01f10610c95ea642'
+  };
